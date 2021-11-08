a2621b97d1a161d389ba46c918c8a728ee8a26f2
Neha9849 <nehadeekonda66@gmail.com>
2021-11-08 23:28:02 +0530

    update

--- server/config/key.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI:"mongodb+srv://neha9849:neha9849@firstcluster.8ai5f.mongodb.net/FirstCluster?retryWrites=true&w=majority"
-}
--- server/index.js
+++ server/index.js
@@ -1,13 +1,19 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const PORT= process.env.PORT || 5000;
-const app = express();
-//middlewares
-app.use(express.json())
-//connect to database
-db= require('./config/key').mongoURI;
-mongoose.connect(db,{useNewUrlParser: true})
-.then(()=>console.log('connected to db'))
-.catch(err=>console.log(err))
+const express = require("express");
+const connectDB = require('./config/db')
 
-app.listen(PORT,()=>console.log('server running'))
+const app = express()
+//connect to db
+connectDB();
+//middlewares
+app.use(express.json());
+//routes
+app.get('/',(req,res)=>{
+  res.send('home')
+})
+app.use('/api/users',require('./routes/api/users'))
+//port
+const PORT = process.env.PORT || 5000;
+//strt server
+app.listen(PORT,()=>{
+  console.log('server started')
+})
