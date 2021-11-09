25053b45dec91fe77373eeb65c41e1e74f651b84
Neha9849 <nehadeekonda66@gmail.com>
2021-11-10 02:30:29 +0530

    registration update

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const auth = require('../../middleware/auth')
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const User = require("./../../models/User");
 //get req
-router.get('/',auth,(req,res)=>{
- res.send('auth')
-})
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+  //res.send('auth')
+});
 module.exports = router;
