6102a449204e5492bbaa6f137e9adfc1bc386556
Neha9849 <nehadeekonda66@gmail.com>
2021-11-09 20:42:19 +0530

    done with validation

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,6 +1,21 @@
 const express = require('express')
 const router = express.Router();
-router.get('/',(req,res)=>{
-    res.send('users route')
+const { body, validationResult } = require('express-validator');
+//post req (to register)
+router.post('/',[
+    body('name').isLength({min:3}).withMessage('name must have atleast 3 chars'),
+    body('email').isEmail().withMessage('Invalid email address'),
+    body('password').isLength({min:8}).withMessage('password must contain 8 or more characters')
+]
+,(req,res)=>{
+    //validation
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors:errors.array()})
+    }
+
+    console.log(req.body)
+    //res
+    res.send('users')
 })
 module.exports = router;
